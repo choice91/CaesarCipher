c98f6007cff4df2d62568aa8cac6838437a716a7
Evan Barry <Evan.Barry@students.ittralee.ie>
2019-02-05 09:59:08 +0000

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+        String word = CaesarCipher.encryptCaesarCipher("I Love Algorithms", -2);
+        System.out.println(word);
+    }
+}
