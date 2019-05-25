fd2b8222aa4a8f3213d52f4fad254ca1eeb3b110
LDH <heon6560@naver.com>
2019-05-26 00:23:29 +0900

    "컴포넌트 객체 오류 해결"

--- src/E_Frame.java
+++ src/E_Frame.java
@@ -1,24 +1,25 @@
 import java.awt.*;
 import java.awt.event.*;
 
-public class E_Frame extends Frame {
+public class E_Frame {
 	TextArea Contents; //파일 내용 출력
 	Panel P1;//텍스트 에어리어만 담음
 	Panel P2;//버튼만 담음
+	Frame f;
 	
 	
 	E_Frame(){
-		super("Encryption");
-		setLayout(null);
-		setSize(500,500);
+		f=new Frame("Encryption");
+		f.setLayout(null);
+		f.setSize(500,500);
 
 		Toolkit tk=Toolkit.getDefaultToolkit();
 		Dimension screenSize=tk.getScreenSize();//화면의 크기를 구함
 
-		setLocation(screenSize.width/2-250,screenSize.height/2-250);
-		setVisible(true);
+		f.setLocation(screenSize.width/2-250,screenSize.height/2-250);
+		f.setVisible(true);
 
-		addWindowListener(new WindowAdapter() {
+		f.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);	
 			}
@@ -27,16 +28,16 @@ public class E_Frame extends Frame {
 		P1=new Panel();
 		P1.setSize(400,200);
 		P1.setLocation(50,50);
-		P1.setBackground(Color.yellow);
+		
 		
 		Contents=new TextArea("내용 입력",10,50);
 		P1.add(Contents);
-		add(P1);
+		f.add(P1);
 		
 		P2=new Panel();
 		P2.setSize(400,200);
 		P2.setLocation(50,300);
-		P2.setBackground(Color.red);
+		
 		Button button1=new Button("파일열기");
 		Button button2=new Button("암호화 하기");
 		Button button3=new Button("복호화 하기");
@@ -45,9 +46,33 @@ public class E_Frame extends Frame {
 		P2.add(button2);
 		P2.add(button3);
 		
-		add(P2);
+		f.add(P2);
 		
+		f.revalidate();
 		
-		
+		button1.addActionListener(new Handler());
+		button2.addActionListener(new Handler());
+		button3.addActionListener(new Handler());
+	}
+	
+	class Handler implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			String name=e.getActionCommand();
+			
+			
+			if(name.equals("파일열기")) {
+				
+				FileDialog fileOpen=new FileDialog(f,"파일열기",FileDialog.LOAD);
+				fileOpen.setDirectory("C:\\");
+				fileOpen.setVisible(true);
+				
+			}
+			else if(name.equals("암호화 하기")) {
+				
+			}
+			else if(name.equals("복호화 하기")) {
+				
+			}
+		}
 	}
 }
