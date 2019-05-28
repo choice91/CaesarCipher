8047d53f165415a3c78d3bc42f8b157d0e2a5631
LDH <heon6560@naver.com>
2019-05-28 22:22:10 +0900

    "AES암호화 구현"

--- src/Handler2.java
+++ src/Handler2.java
@@ -43,8 +43,39 @@ public class Handler2 implements ActionListener {
 				bw2.close();//출력 스트림 종료
 			}catch(IOException IE) {}
 		}
-		
-		
-		
+		else if(name2.equals("AES")) {
+			FileDialog fileOpen=new FileDialog(E_Frame.f,"파일열기",FileDialog.LOAD);
+			fileOpen.setDirectory("D:\\"); //파일 오픈 경로 지정
+			fileOpen.setVisible(true); //파일 창 눈에 보이게 설정
+			path2=fileOpen.getDirectory()+fileOpen.getFile(); //파일의 경로 및 이름을 얻기 위함
+			
+			
+			try {
+				fr2=new FileReader(path2);
+				br2=new BufferedReader(fr2);
+			
+				
+				String filename2=fileOpen.getFile();
+				filename2=filename2.replace(".txt","_Encryption(AES).txt");//원본 파일 이름을 얻어서 암호문 파일 생성 위함
+				File Fi2=new File("D:\\",filename2);
+				Fi2.createNewFile();
+				
+				fw2=new FileWriter(Fi2);//새로 생성된 파일에 스트림 설치
+				bw2=new BufferedWriter(fw2);//파일 스트림에 버퍼 스트림 설치
+				
+				String line="";
+				for(int i=0; (line=br2.readLine())!=null; i++) {
+			        String keyString = "1234567890";
+			        String encryptStr="";
+			        
+			        try {
+			        	encryptStr = AES.encrypt(keyString, line);
+			        }catch(Exception E) {}
+			        bw2.write(encryptStr+"\r\n");
+				}
+				br2.close();//입력 스트림 종료
+				bw2.close();//출력 스트림 종료
+			}catch(IOException IE) {}
+		}	
 	}
 }
